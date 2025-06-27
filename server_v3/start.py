36e4e78d458d441ed1badea17cbd6b5078db3838
DanielDobromylskyj <90842024+DanielDobromylskyj@users.noreply.github.com>
2025-06-27 15:51:01 +0100

    Added Shutdown -> Even on crash

--- server_v3/start.py
+++ server_v3/start.py
@@ -4,4 +4,17 @@ from findabac import server
 
 
 instance = server.WebServer()
+
+def cleanup():
+    instance.shutdown()
+
+def handle_signal(sig, frame):
+    cleanup()
+    sys.exit(0)
+
+
+signal.signal(signal.SIGINT, handle_signal)  # Ctrl+C
+signal.signal(signal.SIGTERM, handle_signal)  # Ctrl+C
+atexit.register(cleanup)  # On Exit
+
 instance.run()
